8e59bf0de25cbe2d1dcd4d193c6ffee2fd1dbe70
Rohan Rohokale <rohanrohokalegit@gmail.com>
2022-09-12 10:28:01 +0530

    inspect-mode-off

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +7,31 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'web-admin';
+  @HostListener('document:keydown', ['$event'])
+  handleKeyboardEvent(e: KeyboardEvent) {
+    console.log(e)
+    if (e.key === 'F12') {
+      return false;
+    }
+    if (e.ctrlKey && e.shiftKey && e.key === "I") {
+      return false;
+    }
+    if (e.ctrlKey && e.shiftKey && e.key === "C") {
+      return false;
+    }
+    if (e.ctrlKey && e.shiftKey && e.key === "J") {
+      return false;
+    }
+    if (e.ctrlKey && e.key == "U") {
+      return false;
+    }
+    return true;
+  }
+
+  constructor() {
+    document.addEventListener('contextmenu', function(e) {
+      e.preventDefault();
+    });
+  }
+
 }
