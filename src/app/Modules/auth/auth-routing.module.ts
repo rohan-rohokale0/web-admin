5d42a04b1d51076837b68c68033f05bf167bfc6b
Rohan Rohokale <rohanrohokalegit@gmail.com>
2022-09-14 23:55:49 +0530

    forgoat-password

--- src/app/Modules/auth/auth-routing.module.ts
+++ src/app/Modules/auth/auth-routing.module.ts
@@ -1,11 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ForgotPasswordComponent } from './Compoents/forgot-password/forgot-password.component';
 import { SignInComponent } from './Compoents/sign-in/sign-in.component';
 import { SignUpComponent } from './Compoents/sign-up/sign-up.component';
 
 const routes: Routes = [
   {
     path: 'sign-up', component: SignUpComponent
+  },
+  {
+    path: 'forgot-password', component: ForgotPasswordComponent
   }
 
 ];
